4b4b5b6d1dd88efe2fecc351d078be8cc25c4f94
sungchun12 <sungwonchung3@gmail.com>
2023-06-06 16:46:01 -0700

    dbt tutorial success

--- tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/__init__.py
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/__init__.py
@@ -1,2 +1,26 @@
 ## Refer to Using dbt with Dagster, part two for info about this file:
 ## https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-two
+# /tutorial_template/tutorial_dbt_dagster/__init__.py
+
+import os
+
+from dagster_dbt import DbtCliClientResource
+from tutorial_dbt_dagster import assets
+from tutorial_dbt_dagster.assets import DBT_PROFILES, DBT_PROJECT_PATH
+
+from dagster_duckdb_pandas import duckdb_pandas_io_manager
+
+from dagster import Definitions, load_assets_from_modules
+
+
+resources = {
+    "dbt": DbtCliClientResource(
+        project_dir=DBT_PROJECT_PATH,
+        profiles_dir=DBT_PROFILES,
+    ),
+    "io_manager": duckdb_pandas_io_manager.configured(
+        {"database": os.path.join(DBT_PROJECT_PATH, "tutorial.duckdb")}
+    ),
+}
+
+defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
--- tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
+++ tutorial_dbt_dagster/tutorial_template/tutorial_dbt_dagster/assets/__init__.py
@@ -1,2 +1,45 @@
 ## Refer to Using dbt with Dagster, part two for info about this file:
 ## https://docs.dagster.io/integrations/dbt/using-dbt-with-dagster/part-two
+
+# /tutorial_template/tutorial_dbt_dagster/assets/__init__.py
+
+import pandas as pd
+import plotly.express as px
+from dagster_dbt import load_assets_from_dbt_project
+
+from dagster import AssetIn, MetadataValue, asset, file_relative_path
+
+
+@asset(key_prefix=["jaffle_shop"], group_name="staging")
+def customers_raw() -> pd.DataFrame:
+    data = pd.read_csv("https://docs.dagster.io/assets/customers.csv")
+    return data
+
+
+@asset(key_prefix=["jaffle_shop"], group_name="staging")
+def orders_raw() -> pd.DataFrame:
+    data = pd.read_csv("https://docs.dagster.io/assets/orders.csv")
+    return data
+
+
+DBT_PROJECT_PATH = file_relative_path(__file__, "../../jaffle_shop")
+DBT_PROFILES = file_relative_path(__file__, "../../jaffle_shop/config")
+
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=DBT_PROJECT_PATH, profiles_dir=DBT_PROFILES, key_prefix=["jaffle_shop"]
+)
+
+
+@asset(
+    ins={"customers": AssetIn(key_prefix=["jaffle_shop"])},
+    group_name="staging",
+)
+def order_count_chart(context, customers: pd.DataFrame) -> None:
+    fig = px.histogram(customers, x="number_of_orders")
+    fig.update_layout(bargap=0.2)
+    save_chart_path = file_relative_path(__file__, "order_count_chart.html")
+    fig.write_html(save_chart_path, auto_open=True)
+
+    context.add_output_metadata(
+        {"plot_url": MetadataValue.url("file://" + save_chart_path)}
+    )
